b17709d43ec4ea475bd163754174339759a66810
MPaladin16 <mp52512@fer.hr>
2022-01-21 12:17:31 +0100

    ureden UI i Svi minigameovi, rasporedene stvari po prvoj sobi

--- ORDI - Escape room/Assets/Scripts/MoveButton.cs
+++ ORDI - Escape room/Assets/Scripts/MoveButton.cs
@@ -8,24 +8,25 @@ public class MoveButton : MonoBehaviour
     RectTransform button;
     Vector3 startingPosition;
     public float speed;
+    public Transform myObj;
 
     void Start()
     {
         button = gameObject.GetComponent<RectTransform>();
         canvas = GameObject.Find("CanvasLab").GetComponent<RectTransform>();
         startingPosition = transform.position;
-        int xcount = Random.Range(4, 12);
+        int xcount = Random.Range(3, 11);
         speed = xcount;
     }
 
     void FixedUpdate()
     {
         if (transform.position.y < 300) {
-            speed = -speed;
+            transform.Rotate (180, 0, 0, Space.World);
         }
         if (transform.position.y > 775)
         {
-            speed = -speed;
+            transform.Rotate(180, 0, 0, Space.World);
         }
         transform.Translate(0f, speed, 0f);
         if (button.position.y < -button.rect.height)
